5b8aaf0c01b55775ad4aff38540396286e3e53aa
Matthew Watkins <mwatkins@linuxfoundation.org>
2024-01-12 12:21:54 +0000

    Feat: Update test renaming script to reverse original operation
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

---
+++ scripts/rename-tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#set -x
+
+REPO_NAME=$(basename $(git rev-parse --show-toplevel))
+echo "Repository name: $REPO_NAME"
+
+if [ $# -ne 1 ]; then
+    echo "Usage:	$0 [test folder]"; exit 1
+elif [ ! -d "$1" ]; then
+    echo "Error: specified target was not a folder"; exit 1
+else
+    # Target specified was a folder
+    TARGET="$1"
+fi
+
+for TEST in $(find "$TARGET" -type f -name '*_test.py' | xargs); do
+    echo "Processing: $TEST"
+    FILE_PATH=$(dirname $TEST)
+    FILE_NAME=$(basename $TEST)
+    STRIPPED=$(echo "$FILE_NAME" | sed "s/_test.py/.py/")
+    echo $STRIPPED
+    echo "  git mv ${TEST} $FILE_PATH/test_${STRIPPED%%}"
+    git mv ${TEST} $FILE_PATH/test_${STRIPPED%%}
+done
+
