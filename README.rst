5cb764d26cc17b140136576ae7c804ff4e27a10c
Matthew Watkins <mwatkins@linuxfoundation.org>
2024-01-14 20:39:00 +0000

    Chore: Initial code drop
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

---
+++ README.rst
@@ -0,0 +1,40 @@
+
+.. image:: https://img.shields.io/badge/OS-Climate-blue
+  :alt: An OS-Climate Project
+  :target: https://os-climate.org/
+
+.. image:: https://img.shields.io/badge/slack-osclimate-brightgreen.svg?logo=slack
+  :alt: Join OS-Climate on Slack
+  :target: https://os-climate.slack.com
+
+.. image:: https://img.shields.io/badge/GitHub-100000?logo=github&logoColor=white
+  :alt: Source code on GitHub
+  :target: https://github.com/os-climate/osc-python-template
+
+.. image:: https://img.shields.io/pypi/v/osc-python-template.svg
+  :alt: PyPI package
+  :target: https://pypi.org/project/osc-python-template
+
+
+.. image:: https://img.shields.io/badge/PDM-Project-purple
+  :alt: Built using PDM
+  :target: https://pdm-project.org/latest/
+
+.. image:: https://img.shields.io/badge/-PyScaffold-005CA0?logo=pyscaffold
+  :alt: Project generated with PyScaffold
+  :target: https://pyscaffold.org/
+
+
+
+===================
+osc-python-template
+===================
+
+Template repository for the OS-Climate Python projects
+
+.. _notes:
+
+Notes
+=====
+
+Placeholder notes content
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# set -x
+set -eu -o pipefail
+
+### Shared functions
+
+
+### Main script entry point
+
+GIT_CMD=$(which git)
+if [ ! -x "$GIT_CMD" ]; then
+    echo "GIT command was not found in PATH"; exit 1
+fi
+
+DEVOPS_REPO="git@github.com:os-climate/devops-toolkit.git"
+REPO_DIR=$(git rev-parse --show-toplevel)
+
+# Change to top-level of GIT repository
+CURRENT_DIR=$(pwd)
+if [ "$REPO_DIR" != "$CURRENT_DIR" ]; then
+    echo "Changing directory to: $REPO_DIR"
+    if ! (cd "$REPO_DIR"); then
+        echo "Error: unable to change directory"; exit 1
+    fi
+fi
+
+# Directory used below MUST match code in bootstrap.yaml
+DEVOPS_DIR=".devops"
+echo "Cloning DevOps repository into: $DEVOPS_DIR"
+git clone "$DEVOPS_REPO" "$DEVOPS_DIR"
+
+echo "Extracting shell code from bootstrap.yaml file..."
+
+EXTRACT="false"
+while read -r LINE; do
+    if [ "$LINE" = "### SHELL CODE START ###" ]; then
+        EXTRACT="true"
+        SHELL_SCRIPT=$(mktemp -t script-XXXXXXXX.sh)
+        touch "$SHELL_SCRIPT"
+        chmod a+x "$SHELL_SCRIPT"
+        echo "Creating shell script: $SHELL_SCRIPT"
+        echo "#!/bin/sh" > "$SHELL_SCRIPT"
+    fi
+    if [ "$EXTRACT" = "true" ]; then
+        echo "$LINE" >> "$SHELL_SCRIPT"
+        if [ "$LINE" = "### SHELL CODE END ###" ]; then
+            echo "Successfully extracted shell script from bootstrap.yaml"
+            break
+        fi
+    fi
+done < "$DEVOPS_DIR"/.github/workflows/bootstrap.yaml
+
+echo "Running extracted shell script code"
+set +eu +o pipefail
+"$SHELL_SCRIPT"
+
+### Tidy up afterwards
+
+echo "Cleaning up..."
+if [ -d "$DEVOPS_DIR" ] && [ -n "$DEVOPS_DIR" ]; then
+    rm -Rf "$DEVOPS_DIR"
+fi
+if [ -f "$SHELL_SCRIPT" ]; then
+    echo "Deleting shell script code: $SHELL_SCRIPT"
+    rm "$SHELL_SCRIPT"
+fi
